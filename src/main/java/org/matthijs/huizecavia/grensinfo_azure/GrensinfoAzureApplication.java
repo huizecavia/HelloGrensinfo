5db97b8bf353f9b02022c312839aa2b2042efd64
Stevens, Matthijs (AV) <MStevens@svb.nl>
2024-07-05 12:49:58 +0200

    start

---
+++ src/main/java/org/matthijs/huizecavia/grensinfo_azure/GrensinfoAzureApplication.java
@@ -0,0 +1,13 @@
+package org.matthijs.huizecavia.grensinfo_azure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrensinfoAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrensinfoAzureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/matthijs/huizecavia/grensinfo_azure/GrensinfoAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package org.matthijs.huizecavia.grensinfo_azure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrensinfoAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
